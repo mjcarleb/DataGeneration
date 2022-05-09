a4b322c72552eb4504afb4bf65a2d2927d1df798
Mark Carlebach <markcarlebach@macbook-pro.myfiosgateway.com>
2022-05-09 09:31:32 -0400

    First commit

---
+++ generate_positions.py
@@ -0,0 +1,26 @@
+"""
+Goals:
+(1) Generate "real" firm positions
+(2) Generate "synthetic" firm positions
+"""
+
+"""
+Real firm positions:
+(1) Agree on attributes of position table
+    - a
+    - b
+    - c
+    
+(2) For each attribute, define "distribution" valid values
+    - a
+    - b
+    - c
+"""
+
+"""
+Synthetic firm positions:
+(1) For each attribute, define "distribution" valid values
+    - a
+    - b
+    - c
+"""
---
+++ generate_trades.py
@@ -0,0 +1,35 @@
+"""
+Goals:
+(1) Generate "real" firm trades
+(2) Generate "real" street trades
+(3) Generate "synthetic" firm trades - with labels for training
+"""
+
+"""
+Real firm trades:
+(1) Agree on attributes of position table
+    - a
+    - b
+    - c
+
+(2) For each attribute, define "distribution" valid values
+    - a
+    - b
+    - c
+"""
+
+"""
+Real street trades:
+(1) For each attribute, define "distribution" valid values
+    - a
+    - b
+    - c
+"""
+
+"""
+Synthetic firm trades:
+(1) For each attribute, define "distribution" valid values
+    - a
+    - b
+    - c
+"""
